b3c3398e4a5187bf61cb99feb871a11d63298cf2
Ludovic Muller <ludovicmuller1@gmail.com>
2021-10-26 16:54:53 +0200

    parse major.minor.patch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let re = Regex::new(
+        r"(?x)v?
+(?P<major>0|[1-9]\d*)  # major
+\.
+(?P<minor>0|[1-9]\d*)  # minor
+\.
+(?P<patch>0|[1-9]\d*)  # patch
+",
+    )
+    .unwrap();
+
+    let version = "v1.2.3";
+    let caps = re.captures(version).unwrap();
+
+    let major = &caps["major"];
+    let minor = &caps["minor"];
+    let patch = &caps["patch"];
+
+    println!("v{}", major);
+    println!("v{}.{}", major, minor);
+    println!("v{}.{}.{}", major, minor, patch);
 }
